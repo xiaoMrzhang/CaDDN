78b46b7e9d51e154d37b221d75ca84e5dfd2c596
xiaoMrzhang <563562675@qq.com>
2021-04-06 00:19:12 -0700

    first trainging config

--- pcdet/models/detectors/caddn.py
+++ pcdet/models/detectors/caddn.py
@@ -1,14 +1,33 @@
 from .detector3d_template import Detector3DTemplate
+import torch
 
 
 class CaDDN(Detector3DTemplate):
     def __init__(self, model_cfg, num_class, dataset):
         super().__init__(model_cfg=model_cfg, num_class=num_class, dataset=dataset)
         self.module_list = self.build_networks()
-
+    # @profile
     def forward(self, batch_dict):
         for cur_module in self.module_list:
             batch_dict = cur_module(batch_dict)
+        # ffe_module, frustum_to_voxel_module, map_to_bev_module, backbone_2d_module, dense_head_module = self.module_list
+
+        # torch.cuda.synchronize()   #增加同步操作
+        # batch_dict = ffe_module(batch_dict)
+        # torch.cuda.synchronize()   #增加同步操作
+
+        # batch_dict = frustum_to_voxel_module(batch_dict)
+        # torch.cuda.synchronize()   #增加同步操作
+
+        # batch_dict = map_to_bev_module(batch_dict)
+        # torch.cuda.synchronize()   #增加同步操作
+
+        # batch_dict = backbone_2d_module(batch_dict)
+        # torch.cuda.synchronize()   #增加同步操作
+
+        # batch_dict = dense_head_module(batch_dict)
+        # torch.cuda.synchronize()   #增加同步操作
+
 
         if self.training:
             loss, tb_dict, disp_dict = self.get_training_loss()
